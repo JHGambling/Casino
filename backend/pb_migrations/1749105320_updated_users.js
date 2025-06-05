fdd74c5a7451f4a523e0db940e0af1fed76a007b
fstmbl <florian.stembal@jhg-traunreut.de>
2025-06-05 08:39:20 +0200

    Dont think you are supposed to do it like this, but added pocketbase

---
+++ backend/pb_migrations/1749105320_updated_users.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_719040989",
+    "hidden": false,
+    "id": "relation1788209604",
+    "maxSelect": 9000,
+    "minSelect": 0,
+    "name": "identity",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("relation1788209604")
+
+  return app.save(collection)
+})
