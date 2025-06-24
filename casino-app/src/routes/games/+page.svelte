97dd9b5479e5da00fc697bd07346a9e9f42abe80
fstmbl <florian.stembal@jhg-traunreut.de>
2025-06-24 08:42:40 +0200

    Started working on the main game page with topbar

--- casino-app/src/routes/games/+page.svelte
+++ casino-app/src/routes/games/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import { CasinoClient } from "casino-sdk";
+    import { goto } from "$app/navigation";
+    import TopBar from "./TopBar.svelte";
+
+    let client: CasinoClient;
+
+    onMount(async () => {
+        client = new CasinoClient("wss://casino-host.stmbl.dev/ws");
+        await client.connect();
+
+        if (!client.auth.isAuthenticated) {
+            goto("/user/login");
+        }
+    });
 </script>
 
 <svelte:head>
     <title>JHGambling</title>
 </svelte:head>
 
-<div class="app"></div>
+<div class="app">
+    <TopBar />
+</div>
 
 <style lang="scss">
     .app {
@@ -21,6 +38,5 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
     }
 </style>
---
+++ casino-app/src/routes/games/TopBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div class="topbar">
+
+</div>
+
+<style lang="scss">
+    .topbar {
+        width: 100vw;
+        height: 4rem;
+
+        background-color: #ffffff;
+    }
+</style>
